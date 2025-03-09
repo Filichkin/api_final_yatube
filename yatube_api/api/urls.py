8c33b906b282035ed946e7e5c13ad501a5a10ef8
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-03-09 13:56:51 +0300

    add urls.py to api app

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,23 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from . import views
+
+
+API_VERSION = 'v1'
+
+v1_router = SimpleRouter()
+
+v1_router.register('posts', views.PostViewSet)
+v1_router.register('groups', views.GroupViewSet)
+v1_router.register(
+    r'^posts/(?P<post_id>\d+)/comments',
+    views.CommentViewSet,
+    basename='comments'
+)
+v1_router.register('follow', views.FollowViewSet, basename='follow')
 
 urlpatterns = [
+    path(f'{API_VERSION}/', include('djoser.urls.jwt')),
+    path(f'{API_VERSION}/', include(v1_router.urls)),
 ]
